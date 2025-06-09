4e59d9adfa0c21dc8499995504a12b4d66f2c0b8
AikenB <bouisaiken@gmail.com>
2025-06-09 14:19:10 -0700

    simplified more

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class Main {
 
@@ -6,44 +6,6 @@ public class Main {
 
     
     public static void main(String[] args){
-        
-        // GradeBook gradeBook = new GradeBook();
-        // GradeBook gradeBook1to6 = new P1to6GradeBook();
-        // GradeBook gradeBook2to7 = new P2to7GradeBook();
-        
 
-        // gradeBook.setP1Grade(90);
-        // gradeBook.setP2Grade(85);
-        // gradeBook.setP3Grade(93);
-        // gradeBook.setP4Grade(95);
-        // System.out.println("Grade average with periods 1-7: " + gradeBook.calculateAverage());
-        // gradeBook1to6.setP1Grade(90);
-        // gradeBook1to6.setP2Grade(85);
-        // gradeBook1to6.setP3Grade(93);
-        // gradeBook1to6.setP4Grade(99);
-        // System.out.println("Grade average with periods 1-6: " + gradeBook1to6.calculateAverage());
-        // gradeBook2to7.setP2Grade(50);
-        // gradeBook2to7.setP3Grade(67);
-        // gradeBook2to7.setP4Grade(75);
-        // System.out.println("Grade average with periods 2-7: " + gradeBook2to7.calculateAverage());
-        // ExampleClass exampleObject = new ExampleClass();
-        Testing testing = new Testing();
-
-       
-
-        
-
-        
-
-        // exampleObject.method();
-        // new NewClass();
-        // new NewClass().test();
-        // new NewClass().createObject().method();
-
-
-        
-
-
-        
     }
 }
